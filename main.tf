d183e7564c12687f219600d9bdd79df625710a10
Lipika Pal <pallipika@gmail.com>
2024-06-15 21:19:09 +0800

    create azure resource group, virtual network and subnet

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "tutorial" {
+  name     = "tutorial-resources"
+  location = "UK South"
+}
+
+resource "azurerm_virtual_network" "tutorial" {
+  name                = "tutorial-vnet"
+  resource_group_name = azurerm_resource_group.tutorial.name
+  location            = azurerm_resource_group.tutorial.location
+  address_space       = ["172.16.0.0/16"]
+}
+
+resource "azurerm_subnet" "tutorial" {
+  name                 = "tutorial-subnet"
+  resource_group_name  = azurerm_resource_group.tutorial.name
+  virtual_network_name = azurerm_virtual_network.tutorial.name
+  address_prefixes     = ["172.16.1.0/24"]
+}
