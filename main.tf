379285f5dc532a6dc36ecdf0c9e947f637d67dec
lipika <pallipika@gmail.com>
2024-06-17 03:10:56 +0800

    Update jenkinsfile

--- main.tf
+++ main.tf
@@ -25,28 +25,3 @@ module "ec2" {
   vpc_id_test    = module.network.vpc_id_test
   subnet_id_test = module.network.subnet_id_test
 }
-
-
-
-
-
-
-# resource "azurerm_resource_group" "tutorial" {
-#   name     = "${var.project}-${var.environment}-rg"
-#   location = var.location
-#   tags = local.tags
-# }
-#
-# resource "azurerm_virtual_network" "tutorial" {
-#   name                = "tutorial-vnet"
-#   resource_group_name = azurerm_resource_group.tutorial.name
-#   location            = azurerm_resource_group.tutorial.location
-#   address_space       = ["172.16.0.0/16"]
-# }
-#
-# resource "azurerm_subnet" "tutorial" {
-#   name                 = "tutorial-subnet"
-#   resource_group_name  = azurerm_resource_group.tutorial.name
-#   virtual_network_name = azurerm_virtual_network.tutorial.name
-#   address_prefixes     = ["172.16.1.0/24"]
-# }
