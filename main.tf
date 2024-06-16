c2ef129f19cced27643678e4effa8d3cf92f5b2e
lipika <pallipika@gmail.com>
2024-06-16 18:24:56 +0800

    tutorial

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   profile = "default"
   region                  = var.region
-  shared_credentials_file = "~/.aws/credentials"
+#   shared_credentials_file = "~/.aws/credentials"
 }
 
 #-----------network module-----------------
